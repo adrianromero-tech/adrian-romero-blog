83b165b970ae109b872856e6c0b7dea393f99ef4
Adrian Romero <adrianromero.contact@gmail.com>
2024-07-01 15:33:04 -0300

    Makes navigation bar responsive

--- src/app/components/navigationBar/index.tsx
+++ src/app/components/navigationBar/index.tsx
@@ -1,6 +1,22 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  useMediaQuery,
+  IconButton,
+  Container,
+} from '@mui/material';
+import { Menu as MenuIcon, Close as CloseIcon } from '@mui/icons-material';
+import { useState } from 'react';
+import './index.css';
 
 function NavigationBar() {
+  const isDesktopScreen = useMediaQuery('(min-width:600px)');
+  return isDesktopScreen ? <DesktopNavigationBar /> : <MobileNavigationBar />;
+}
+
+function DesktopNavigationBar() {
   return (
     <AppBar position="relative" sx={{ bgcolor: '#6F4E37' }}>
       <Toolbar variant="dense">
@@ -10,6 +26,30 @@ function NavigationBar() {
         <NavigationTab dest="/contact"> contact </NavigationTab>
       </Toolbar>
     </AppBar>
+  );
+}
+
+function MobileNavigationBar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Container className={`mobile-nav-bar ${open ? 'visible' : ''}`}>
+        <IconButton onClick={() => setOpen(false)}>
+          <CloseIcon />
+        </IconButton>
+        <NavigationTab dest="/home"> home </NavigationTab>
+        <NavigationTab dest="/about"> about </NavigationTab>
+        <NavigationTab dest="/archive"> archive </NavigationTab>
+        <NavigationTab dest="/contact"> contact </NavigationTab>
+      </Container>
+      <AppBar position="fixed" sx={{ bgcolor: '#6F4E37' }}>
+        <Toolbar variant="dense">
+          <IconButton onClick={() => setOpen(true)}>
+            <MenuIcon sx={{ color: 'seashell' }} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
 
